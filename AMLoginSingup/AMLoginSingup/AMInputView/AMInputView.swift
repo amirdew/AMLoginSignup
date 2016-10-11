150290a4e737d1058149a38f822b53fef96737d1
amir kh <khorsandi@me.com>
2016-10-11 15:33:35 +0330

    add gitignore

---
+++ AMLoginSingup/AMLoginSingup/AMInputView/AMInputView.swift
@@ -0,0 +1,21 @@
+//
+//  AMInputView.swift
+//  AMLoginSingup
+//
+//  Created by amir on 10/11/16.
+//  Copyright © 2016 amirs.eu. All rights reserved.
+//
+
+import UIKit
+
+class AMInputView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AMLoginSingup/AMLoginSingup/extentions.swift
@@ -0,0 +1,9 @@
+//
+//  extentions.swift
+//  AMLoginSingup
+//
+//  Created by amir on 10/11/16.
+//  Copyright © 2016 amirs.eu. All rights reserved.
+//
+
+import Foundation
